6bffe13b07b53b43468d32e1fcbe720e8d199cb8
Chris Wan <wpy.christopher@gmail.com>
2025-01-13 08:21:13 +0800

    Extract common webpack configuration

--- apps/world/webpack.config.js
+++ apps/world/webpack.config.js
@@ -1,95 +1,17 @@
-const autoprefixer = require("autoprefixer");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const config = require("../../webpack.base");
 
+
+/** @type {import('webpack').Configuration} */
 module.exports = {
+  ...config,
   entry: {
     world: './src/App.tsx'
   },
-
   output: {
     publicPath: 'http://localhost:8081/'
   },
-
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-
   devServer: {
-    port: 8081,
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 2
-            }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                ident: "postcss",
-                plugins: [autoprefixer()]
-              }
-            }
-          },
-          "sass-loader"
-        ],
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        // https://babeljs.io/docs/en/config-files#root-babelconfigjson-file
-        use: {
-          loader: "babel-loader",
-          options: {
-            rootMode: "upward"
-          }
-        }
-      }
-    ]
-  },
-
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html"
-    })
-  ],
-
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        reactVendor: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: "react-vendor"
-        },
-        reduxVendor: {
-          test: /[\\/]node_modules[\\/](@reduxjs[\\/]toolkit|react-redux)[\\/]/,
-          name: "redux-vendor"
-        },
-        utilityVendor: {
-          test: /[\\/]node_modules[\\/](lodash|moment|moment-timezone)[\\/]/,
-          name: "utility-vendor"
-        },
-        vendor: {
-          test: /[\\/]node_modules[\\/](!lodash)(!moment)(!moment-timezone)[\\/]/,
-          name: "vendor"
-        }
-      }
-    }
+    ...config.devServer,
+    port: 8081
   }
 };
---
+++ webpack.base.js
@@ -0,0 +1,93 @@
+const autoprefixer = require("autoprefixer");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  output: {
+    publicPath: 'http://localhost:8080/'
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: [autoprefixer()]
+              }
+            }
+          },
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        // https://babeljs.io/docs/en/config-files#root-babelconfigjson-file
+        use: {
+          loader: "babel-loader",
+          options: {
+            rootMode: "upward"
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        reactVendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: "react-vendor"
+        },
+        reduxVendor: {
+          test: /[\\/]node_modules[\\/](@reduxjs[\\/]toolkit|react-redux)[\\/]/,
+          name: "redux-vendor"
+        },
+        utilityVendor: {
+          test: /[\\/]node_modules[\\/](lodash|moment|moment-timezone)[\\/]/,
+          name: "utility-vendor"
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/](!lodash)(!moment)(!moment-timezone)[\\/]/,
+          name: "vendor"
+        }
+      }
+    }
+  }
+};
